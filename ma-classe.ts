242251c64b967c6097f3daf5d93f96a3ba74ab29
toure <tourekadiy@gmail.com>
2017-09-06 10:33:56 +0200

    installation du serveur express & body parser;

--- ma-classe.ts
+++ ma-classe.ts
@@ -4,10 +4,10 @@ On peut faire en typescript des classes assez similaires
 à celle du PHP avec des propriétés typées, des visibilités,
 
 */
-class MaClasse {
+export class MaClasse {
 private maPropriete:string;
 
-//Méthode de construction, équivalente à __construct
+//Méthode de construction, équivalente à __construct()
 constructor(){
      this.maPropriete = 'bloup';
 }
@@ -16,3 +16,4 @@ methode():string {
     return 'la propriété vaut : '+this.maPropriete;
 }
 }
+
---
+++ main.ts
@@ -0,0 +1,12 @@
+let express = require('express'),
+app = express();
+
+app.listen(8081, function(err) {
+    if (err) {
+        console.error("Error:", err);
+        return;
+    }
+    console.log('Serveur lançé sur le port 8081');
+});
+
+let bodyParser = require('body-parser')
