8d3072546f2da10ca59073717923cb4e639d3501
toure <tourekadiy@gmail.com>
2017-09-08 10:58:03 +0200

    creation routes;

---
+++ todo-router.ts
@@ -0,0 +1,14 @@
+const todo = new Todo();
+export const router = express.Router();
+
+router.get('/',(req,res) => res.json(todo.lister()));
+router.post('/',(req,res) => {
+    let nouveau = req.body.nouveau;
+    todo.ajouter(nouveau);
+    res.end('todo ajouté');
+});
+router.delete('/:suppr', (req,res) => {
+    let suppr = req.params.suppr;
+    todo.supprimer(suppr);
+    res.end('todo supprimé');
+});
