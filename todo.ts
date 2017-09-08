c3aa555012211e1e0ec6721fb881ff80b1a15b35
toure <tourekadiy@gmail.com>
2017-09-08 10:14:45 +0200

    todo.ts => class todo qui gère la todolist;

---
+++ todo.ts
@@ -0,0 +1,13 @@
+class Todo {
+private liste: string[] = [];
+ajouter(item: string): void {
+    this.liste.push(item);
+}
+supprimer(item: string): void {
+    this.liste = 
+         this.liste.filter((valeur) => valeur !== item);
+}
+        lister(): string[]{
+            return this.liste;
+        }
+}
